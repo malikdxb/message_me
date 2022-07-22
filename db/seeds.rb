0bda06ca4406f356aecbfdd616f1bf1171e00715
malikdxb <107611944+malikdxb@users.noreply.github.com>
2022-07-22 16:00:43 +0400

    build user resource
    making different users easily with seed.rb
    and convert in table form with Hirb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+User.create(username: "malik", password: "password")
+User.create(username: "J.B", password: "password")
+User.create(username: "Rayo", password: "password")
+User.create(username: "Jack", password: "password")
+User.create(username: "Deny", password: "password")
