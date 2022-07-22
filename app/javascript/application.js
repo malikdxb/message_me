fc6048ab8d16e50a0d16f0e3a2892ef94bd4cc40
malikdxb <107611944+malikdxb@users.noreply.github.com>
2022-07-22 12:42:54 +0400

    add favicon
    adding image to app name

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,7 +2,11 @@
 import "@hotwired/turbo-rails"
 import "controllers"
 //= require jquery
+//= require jquery_ujs
 //= require semantic-ui
+//= require bootstrap.min
+// require turbolinks
+//= require_tree
 
 $(document).on('turbolinks:load', function() {
     $('.ui.dropdown').dropdown();
